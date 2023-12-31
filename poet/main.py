44ad200e91bb8cb2b6a016e99426910cfe293010
yganalyst <ygshin0208@gmail.com>
2023-12-31 16:31:21 +0900

    Edit

--- poet/main.py
+++ poet/main.py
@@ -1,5 +1,5 @@
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 from langchain.chat_models import ChatOpenAI
 import streamlit as st
 
@@ -7,10 +7,9 @@ chat_model = ChatOpenAI()
 
 st.title('인공지능 시인')
 
-content = "수학"
-result = chat_model.predict(content+"에 대한 시를 써줘")
-
 title = st.text_input('시의 주제를 제시해주세요.')
 
 if st.button('요청하기'):
-    st.write(result)
+    with st.spinner('시를 작성 중입니다...'):
+        result = chat_model.predict(title + "에 대한 시를 작성해줘")
+        st.write(result)
